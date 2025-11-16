292651d357883c35cf7e5bd1dbcd95a99c24b913
itaym-intel <itay.mevorach@intel.com>
2025-11-16 06:57:29 -0500

    bert service + wrapper for search function

--- infrastructure/embedding/__init__.py
+++ infrastructure/embedding/__init__.py
@@ -1,4 +1,17 @@
 from .imagebind_service import EmbeddingResult, ImageBindService, get_embedding_service
+from .sentence_bert_service import (
+    SentenceBertService,
+    TextEmbeddingResult,
+    get_sentence_bert_service,
+)
 
-__all__ = ["EmbeddingResult", "ImageBindService", "get_embedding_service"]
-
+__all__ = [
+    # ImageBind (multimodal, heavy)
+    "EmbeddingResult",
+    "ImageBindService",
+    "get_embedding_service",
+    # Sentence-BERT (text-only, lightweight)
+    "SentenceBertService",
+    "TextEmbeddingResult",
+    "get_sentence_bert_service",
+]
